ec12528ce7dd161171ec2a59db2a59c83d17a73a
xubiao <xubiaosunny@163.com>
2017-12-04 20:10:03 +0800

    20171204

---
+++ python/object/vector_v1.py
@@ -0,0 +1,97 @@
+#-*- coding: utf-8 -*-
+from array import array
+import reprlib
+import math, numbers
+import functools
+import operator
+
+class Vector:
+    typecode = 'd'
+    shortcut_names = 'xyzt'
+
+    def __init__(self, components):
+        self._components = array(self.typecode, components)
+
+    def __iter__(self):
+        return iter(self._components)
+
+    def __repr__(self):
+        components = reprlib.repr(self._components)
+        components = components[components.find('['):-1]
+        return 'Vector({})'.format(components)
+
+    # def __str__(self):
+    #     return str(tuple(self))
+
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) + bytes(self._components))
+
+    def __eq__(self, other):
+        return tupe(self) == tuple(other)
+
+    def __abs__(self):
+        return math.sqrt(sum(x * x for x in self))
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:].cast(typecode))
+        return cls(memv)
+
+    def __len__(self):
+        return len(self._components)
+
+    def __getitem__(self, index):
+        cls = type(self)
+        if isinstance(index, slice):
+            return cls(self._components[index])
+        elif isinstance(index, numbers.Integral):
+            return self._components[index]
+        else:
+            msg = '{cls.__name__} indices nust be integers'
+            raise TypeError(msg.format(cls=cls))
+
+    def __getattr__(self, name):
+        cls = type(self)
+        if len(name) == 1:
+            pos = cls.shortcut_names.find(name)
+            if 0 <= pos < len(self._components):
+                return self._components[pos]
+        msg = '{.__name__!r} object has no attribute {!r}'
+        raise AttributeError(msg.format(cls, name))
+
+    def __setattr__(self, name, value):
+        cls= type(self)
+        if len(name) == 1:
+            if name in cls.shortcut_names:
+                error = 'readonlyattrbute {attr_name!r}'
+            elif name.islower():
+                error = "cant't set attribute 'a' to 'z' in {cls_name!r}"
+            else:
+                error = ''
+            if error:
+                msg = error.format(cls_name=cls.__name__, attr_name=name)
+                raise AttributeError(msg)
+        super().__setattr__(name, value)
+
+    def __eq__(self, other):
+        return len(self) == len(other) and all(a == b for a, b in zip(self, other))
+
+    def __hash__(self):
+        hashes = map(hash, self._components)
+        return functools.reduce(operator.xor, hashes)
+
+
+
+if __name__ == "__main__":
+    print(Vector([3.1, 4.2]))
+    print(Vector([3, 4, 5]))
+    print(Vector(range(10)))
+    v1 = Vector(range(7))
+    print(v1[0], v1[-1], v1[::2])
+    print(v1.x, v1.y, v1.z)
+    # v1.x = 2
+    print(hash(v1))
